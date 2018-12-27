193f81763c37bbfb14bbc0e15b6891049477633a
José Andrés Ulloa <Jose Andres Ulloa@ln.corp.nacion.com>
2018-12-27 17:43:28 -0600

    acomodar la vara

--- examples/math.js
+++ examples/math.js
@@ -52,8 +52,8 @@ const trainingData = [
 
 const net = new brain.recurrent.LSTM({ hiddenLayers: [20] });
 
-net.train(trainingData, { errorThresh: 0.025 });
+net.train(trainingData, { errorThresh: 0.0025 });
 
-console.log(net.run('0+1='));
+console.log(net.run('0+5='));
 console.log(net.run('4+1='));
 console.log(net.run('2+1='));
